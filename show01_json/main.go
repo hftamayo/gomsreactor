6588a2af83dbfef4c1d34ee30597c2526ce6840a
hftamayo <hftamayo@gmail.com>
2023-05-03 19:00:25 -0600

    coding function to code and show data in json format

---
+++ show01_json/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+
+func main(){
+	encodeJson()
+}
+
+func encodeJson(){
+	person := Person{Name: "John", Age: 32}
+
+	jsonData, err:= json.Marshal(person)
+	if err!= nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(jsonData))
+}
